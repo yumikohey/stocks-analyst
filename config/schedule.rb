7673956c9043796212eccf6c042b254a129a19f4
Yumiko Huang <dayumikoda@gmail.com>
2017-08-20 17:48:40 -0500

    testing cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every :day, :at => '10:40pm' do
-    rake "historical_data:collect_historical_data"
+# every :day, :at => '10:40pm' do
+#     rake "historical_data:collect_historical_data"
+# end
+
+every :day, :at => '10:55pm' do
+    rake "test:test"
 end
